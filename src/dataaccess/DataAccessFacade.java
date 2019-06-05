b3fb3d333afd054116f8dc3e66c861007f1d07a1
james alituhikya <james.alituhikya@gmail.com>
2019-06-05 22:29:30 +0300

    added upddate book to DataAccessFacade

--- src/dataaccess/DataAccessFacade.java
+++ src/dataaccess/DataAccessFacade.java
@@ -22,7 +22,7 @@ public class DataAccessFacade implements DataAccess {
 		BOOKS, MEMBERS, USERS;
 	}
 
-	public static final String OUTPUT_DIR = System.getProperty("user.dir") + "/src/dataaccess/storage";
+	public static final String OUTPUT_DIR = System.getProperty("user.dir") + "\\src\\dataaccess\\storage";
 	public static final String DATE_PATTERN = "MM/dd/yyyy";
 
 	private void saveMember(LibraryMember member) {
